85528206f35b57e7a4a139c663114aadb9970a53
JohnsonChen <chenjohnson.tw@gmail.com>
2016-10-17 17:32:32 +0800

    My Xblock example , first commit

---
+++ myxblock/static/js/src/myxblock.js
@@ -0,0 +1,33 @@
+/* Javascript for MyXBlock. */
+function MyXBlock(runtime, element) {
+
+    function updateCount(result) {
+        $('.count', element).text(result.count);
+    }
+
+    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+
+    $("#myDiv").click(function(eventObject) {
+        $.ajax({
+            type: "POST",
+            url: handlerUrl,
+            data: JSON.stringify({"hello": "world"}),
+            success: updateCount
+        });
+    });
+
+    /*
+    $('p', element).click(function(eventObject) {
+        $.ajax({
+            type: "POST",
+            url: handlerUrl,
+            data: JSON.stringify({"hello": "world"}),
+            success: updateCount
+        });
+    });
+    */
+
+    $(function ($) {
+        /* Here's where you'd do things on page load. */
+    });
+}
