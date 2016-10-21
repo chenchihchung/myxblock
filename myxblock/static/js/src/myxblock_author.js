90856a3f3a283f0d8d55bd8b408ddbbef2f0edba
JohnsonChen <chenjohnson.tw@gmail.com>
2016-10-21 15:28:10 +0800

    test1

--- myxblock/static/js/src/myxblock_author.js
+++ myxblock/static/js/src/myxblock_author.js
@@ -5,12 +5,12 @@ function MyXBlock(runtime, element) {
 
     function getInit(result) {
         access_token = result.access_token;
+        console.log('access_log -->'+access_token);
         $('#access_token').text(result.returndata);
         //$('#access_token', element).text(result.returndata);
     }
 
     function author_init() {
-        alert(" author_init() ");
         var url = runtime.handlerUrl(element, 'get_init');
         $.ajax({
             type: "POST",
@@ -26,7 +26,7 @@ function MyXBlock(runtime, element) {
         /* Here's where you'd do things on page load. */
         alert('This is myxblock_author.js')
         //$(".xblock-display-name").html("xblock title");
-        //$('#access_token').text("abcef");
+        $('#access_token').text("access_token");
 
         author_init();
 
