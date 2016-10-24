17e02784cd81cd8043fd4a4b3200b565f24eef92
JohnsonChen <chenjohnson.tw@gmail.com>
2016-10-24 13:57:41 +0800

    modify

--- myxblock/static/js/src/myxblock_author.js
+++ myxblock/static/js/src/myxblock_author.js
@@ -45,18 +45,51 @@ function MyXBlock(runtime, element) {
     }
 
     function author_init() {
-        var url = runtime.handlerUrl(element, 'get_token');
+        //var url = runtime.handlerUrl(element, 'get_token');
+        //$.ajax({
+        //    type: "POST",
+        //    url: url,
+        //    data: JSON.stringify({"hello": "world"}),
+        //    success: listFiles
+        //});
+    }
+
+    function getAccessToken(result) {
+        console.log(" getAccessToken -->"+result);
+        access_token = result.returndata;
+        //console.log('access_log -->'+access_token);
+        //get_root_uri(access_token);
+        //$('#access_token').text(result.returndata);
+        //$('#access_token', element).text(result.returndata);
+
+    }
+
+    function getToken() {
+        var url = 'https://wamsprodglobal001acs.accesscontrol.windows.net/v2/OAuth2-13';
+        headers = {'Content-Type ': 'application/x-www-form-urlencoded',
+                   'Host': 'wamsprodglobal001acs.accesscontrol.windows.net',
+                   'Content-Length': '120',
+                   'Expect': '100-continue',
+                   'Connection': 'Keep-Alive',
+                   'Accept': 'application/json'};
+        data = {
+                'grant_type': 'client_credentials',
+                'client_id': 'drcedx',
+                'scope': 'urn:WindowsAzureMediaServices',
+                'client_secret': 'oYVh8L+h8DieJ/HgEf6rNo4sohyxdGRV3SLP0oOBK5s='};
         $.ajax({
             type: "POST",
             url: url,
-            data: JSON.stringify({"hello": "world"}),
-            success: listFiles
+            headers : headers,
+            data : data,
+            success : getAccessToken()
         });
     }
 
     $(function ($) {
         /* Here's where you'd do things on page load. */
         console.log('This is myxblock_author.js');
-        author_init();
+        //author_init();
+        getToken();
     });
 }
