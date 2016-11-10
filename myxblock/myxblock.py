74d6a5e281abeb9e5c199e85f17864979c777ed5
chenchihcnung <chenjohnson.tw@gmail.com>
2016-11-10 13:20:12 +0800

    master modify

--- myxblock/myxblock.py
+++ myxblock/myxblock.py
@@ -17,6 +17,7 @@ class MyXBlock(XBlock):
     proxies = {
         #'http': 'http://10.10.1.10:3128',
         #'https': 'http://10.10.1.10:1080',
+	#
         'http'  : '',
         'https' : '',
     }
